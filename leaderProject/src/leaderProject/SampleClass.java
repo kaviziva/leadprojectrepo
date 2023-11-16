a9ad823c92906c78331920f8cd48a47e33fa7194
Sanjit Rajesh <kavitha.vee@gmail.com>
2023-11-16 13:24:41 +0400

    commited changes for leader project branch

--- leaderProject/src/leaderProject/SampleClass.java
+++ leaderProject/src/leaderProject/SampleClass.java
@@ -3,5 +3,6 @@ package leaderProject;
 public class SampleClass {
 public static void main(String args) {
 	System.out.println("project created by lead...");
+	System.out.println("added code for branch");
 }
 }
